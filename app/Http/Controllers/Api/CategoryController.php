3143aab4bdf24a575bd47aed07bd4ba76d565e11
David Stern <20023188@tafe.wa.edu.au>
2021-07-01 22:51:18 +0800

    API: edit a category

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -9,6 +9,12 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
+    protected $validationRules = [
+        "name" => "required|string|max:32",
+        "description" => "max:255",
+        "uploadFile" => "image|mimes:jpg,png,jpeg,gif,svg|max:2048"
+    ];
+
     /**
      * Display a listing of the resource.
      *
@@ -44,13 +50,41 @@ class CategoryController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param  Request  $request
+     * @param  Category  $category
+     * @return JsonResponse
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Category $category)
     {
-        //
+        // Basic validation
+        $this->validate($request, $this->validationRules);
+
+        // If parent category is specified, validate it exists in the database table
+        if ($request->get("parent_category_id")) {
+            $this->validate($request, [
+                "parent_category_id" => "exists:categories,id",
+            ]);
+        }
+
+        // Get all data from the request, and generate a slug
+        $data = $request->all();
+        $data['slug'] = $this->slugify($request->get('name'), 40);
+        // Check if slug is already used
+        if (Category::where('id', '<>', $category->id)->where('slug', $data['slug'])->first()) {
+            // prepend with id to make it unique
+            $data['slug'] = $this->slugify($category->id . "-" . $request->get('name'), 40);
+        }
+
+        // If a file was uploaded, put it in the public/images folder under a timestamped name
+        if ($request->hasFile('uploadFile')) {
+            $fileName = time().'_'.$request->file('uploadFile')->getClientOriginalName();
+            $request->file('uploadFile')->move(public_path('images'), $fileName);
+            $data['picture'] = $fileName;
+        }
+
+        $updated = $category->update($data);
+
+        return response()->json(compact('updated', 'category'));
     }
 
     /**
