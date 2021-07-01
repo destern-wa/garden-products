f44a36a288976477b2bc022ac2b96919509fb375
David Stern <20023188@tafe.wa.edu.au>
2021-07-01 20:11:13 +0800

    Protect admin routes (require authentication)

--- routes/web.php
+++ routes/web.php
@@ -20,12 +20,7 @@ Route::view('/contact', 'customer.contact')->name('contact');
 Route::get('/products', '\App\Http\Controllers\CustomerController@products')->name('products');
 
 /* Admin portal routes */
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    // Redirect the /dashboard route (which Jetstream uses) to the /admin (dashboard) route
-    return redirect(route('dashboard'));
-});
-
-Route::prefix('admin')->group(function () {
+Route::middleware(['auth:sanctum', 'verified'])->prefix('admin')->group(function () {
     Route::view('/', 'admin.dashboard')->name('dashboard');
 
     Route::resource('/categories', '\App\Http\Controllers\CategoryController');
@@ -33,4 +28,11 @@ Route::prefix('admin')->group(function () {
 
     Route::resource('/products', '\App\Http\Controllers\ProductController');
     Route::get('/products/{product}/delete', '\App\Http\Controllers\ProductController@delete')->name('products.delete');
+});
+
+/* Redirect the `/dashboard` route (which Jetstream uses) to the `/admin` route (the actual admin dashboard)
+ * In the future, when there are both admin and regular users, this route could be used as a general user dashboard.
+ */
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return redirect(route('dashboard'));
 });
