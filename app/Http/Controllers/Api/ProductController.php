d8c0352d101bdf52ec1b1137f642051fac092035
David Stern <20023188@tafe.wa.edu.au>
2021-07-02 09:37:03 +0800

    API: edit a product

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -6,9 +6,23 @@ use App\Http\Controllers\Controller;
 use App\Models\Product;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class ProductController extends Controller
 {
+    /**
+     * @var string[] Rules for validating store/update requests
+     */
+    protected $validationRules = [
+        'name' => 'required|max:64',
+        'description' => 'max:255',
+        'price' => 'required|numeric|min:0',
+        'pricing_unit' => 'max:16',
+        'category_id' => 'required|min:0|exists:categories,id',
+        'picture' => 'max:255',
+        "uploadFile" => "image|mimes:jpg,png,jpeg,gif,svg|max:2048"
+    ];
+
     /**
      * Display a listing of the resource.
      *
@@ -22,7 +36,7 @@ class ProductController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -44,13 +58,36 @@ class ProductController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param Product $product
+     * @return JsonResponse
+     * @throws ValidationException
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Product $product)
     {
-        //
+        $this->validate($request, $this->validationRules);
+
+        // Get all data from the request, and generate a slug
+        $data = $request->all();
+        $data['slug'] = $this->slugify($request->get('name'), 72);
+        // Check if slug is already used
+        if (Product::where('id', '<>', $product->id)->where('slug', $data['slug'])->first()) {
+            // prepend with id to make it unique
+            $data['slug'] = $this->slugify($product->id . "-" . $request->get('name'), 72);
+        }
+        // available needs to be a boolean instead of a string
+        $data['available'] = $request->boolean('available');
+
+        // If a file was uploaded, put it in the public/images folder under a timestamped name
+        if ($request->hasFile('uploadFile')) {
+            $fileName = time().'_'.$request->file('uploadFile')->getClientOriginalName();
+            $request->file('uploadFile')->move(public_path('images'), $fileName);
+            $data['picture'] = $fileName;
+        }
+
+        $updated = $product->update($data);
+
+        return response()->json(compact('updated', 'product'));
     }
 
     /**
